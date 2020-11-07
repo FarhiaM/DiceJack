a2856b51a618c80d1ef2e5b7b15ff277e44f6461
Farhia <farhia.fcb@gmail.com>
2020-11-07 11:23:25 +0100

    Initial Commit

---
+++ DiceJack/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DiceJack
+//
+//  Created by Farhia Mohamed Muse on 2020-11-07.
+//  Copyright © 2020 Farhia Mohamed Muse. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
